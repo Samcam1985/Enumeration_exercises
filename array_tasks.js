7436b40c74ea30258dab8c27b61050090ae8cbde
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-26 16:39:40 +0100

    Concat test passed

--- array_tasks.js
+++ array_tasks.js
@@ -1,6 +1,15 @@
 var arrayTasks = {
 
 	concat: function (arr1, arr2) {
+		var concatArray = [];
+
+		for (var item of arr1) {
+			concatArray.push(item);
+		}
+		for (var item of arr2) {
+			concatArray.push(item);
+		}
+		return concatArray;
 
 	},
 
